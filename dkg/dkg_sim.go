53f182c894ca010307ebcb26e2c29fd1980c5870
0xmuralik <krishna192reddy@gmail.com>
2024-02-27 19:10:30 -0700

    dkg simulation

---
+++ dkg/dkg_sim.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	group "github.com/bytemare/crypto"
+
+	"github.com/bytemare/frost"
+	"github.com/bytemare/frost/dkg"
+)
+
+var (
+	participantGeneratedInDKG *frost.Participant
+
+	groupPublicKeyGeneratedInDKG *group.Element
+)
+
+func main() {
+	// Each participant must be set to use the same configuration.
+	maximumAmountOfParticipants := 9
+	threshold := 6
+	configuration := frost.Ristretto255.Configuration()
+
+	// Step 1: Initialise your participant. Each participant must be given an identifier that MUST be unique among
+	// all participants. For this example, this participant will have id = 1.
+	participantIdentifiers := []*group.Scalar{}
+	dkgParticipants := []*dkg.Participant{}
+	for i:=0; i<maximumAmountOfParticipants; i++ {
+		participantIdentifier := configuration.IDFromInt(i+1)
+		dkgParticipant := dkg.NewParticipant(
+			configuration.Ciphersuite,
+			participantIdentifier,
+			maximumAmountOfParticipants,
+			threshold,
+		)
+		participantIdentifiers=append(participantIdentifiers,participantIdentifier)
+		dkgParticipants=append(dkgParticipants,dkgParticipant)
+	}
+	
+
+	// Step 2: Call Init() on each participant. This will return data that must be broadcast to all other participants
+	// over a secure channel.
+	// Step 3: First, collect all round1Data from all other participants. Then call Continue() on each participant
+	// providing them with the compiled data.
+
+	accumulatedRound1Data := make([]*dkg.Round1Data, 0, maximumAmountOfParticipants)
+	
+	for i:=0; i<maximumAmountOfParticipants; i++{
+		round1Data := dkgParticipants[i].Init()
+		if round1Data.SenderIdentifier.Equal(participantIdentifiers[i]) != 1 {
+			panic("this is just a test, and it failed")
+		}
+		accumulatedRound1Data = append(accumulatedRound1Data, round1Data)
+	}
+	
+
+	
+	
+
+	// This will return a dedicated package for each other participant that must be sent to them over a secure channel.
+	// The intended receiver is specified in the returned data.
+	// We ignore the error for the demo, but execution MUST be aborted upon errors.
+	// Step 3: First, collect all round2Data from all other participants. Then call Finalize() on each participant
+	// providing the same input as for Continue() and the collected data from the second round2.
+	accumulatedRound2Data := make(map[string][]*dkg.Round2Data)
+	for i:=0; i<maximumAmountOfParticipants; i++ {
+		round2Data, err := dkgParticipants[i].Continue(accumulatedRound1Data)
+		if err != nil {
+			panic(err)
+		} else if len(round2Data) != len(accumulatedRound1Data)-1 {
+			panic("this is just a test, and it failed")
+		}
+		for i:=range round2Data{
+			receiverString:=hex.EncodeToString(round2Data[i].ReceiverIdentifier.Encode())
+			accumulatedRound2Data[receiverString]=append(accumulatedRound2Data[receiverString],round2Data[i])
+		}
+	}
+
+		
+	for i:=0; i<maximumAmountOfParticipants; i++ {
+		// This will, for each participant, return their secret key (which is a share of the global secret signing key),
+		// the corresponding verification key, and the global public key.
+		// We ignore the error for the demo, but execution MUST be aborted upon errors.
+		participantIdString:=hex.EncodeToString(participantIdentifiers[i].Encode())
+		var participantsSecretKey *group.Scalar
+		participantsSecretKey, _, groupPublicKeyGeneratedInDKG, err := dkgParticipants[i].Finalize(
+			accumulatedRound1Data,
+			accumulatedRound2Data[participantIdString],
+		)
+		if err != nil {
+			fmt.Println("ERROR HERE")
+			panic(err)
+		}
+
+		// It is important to set the group's public key.
+		configuration.GroupPublicKey = groupPublicKeyGeneratedInDKG
+
+		// Now you can build a Signing Participant for the FROST protocol with this ID and key.
+		participantGeneratedInDKG = configuration.Participant(participantIdentifiers[i], participantsSecretKey)
+
+		fmt.Printf("Signing keys for participant set up. ID: %s\n", hex.EncodeToString(participantIdentifiers[i].Encode()))
+
+	}	
+}
---
+++ dkg/go.mod
@@ -0,0 +1,20 @@
+module github.com/jaydenwindle/ethdenver-2024/dkg
+
+go 1.21.0
+
+require (
+	github.com/bytemare/crypto v0.5.2
+	github.com/bytemare/frost v0.0.0-20231127024126-bc6f7874b253
+)
+
+require (
+	filippo.io/edwards25519 v1.0.0 // indirect
+	filippo.io/nistec v0.0.2 // indirect
+	github.com/bytemare/hash v0.1.5 // indirect
+	github.com/bytemare/hash2curve v0.2.2 // indirect
+	github.com/bytemare/secp256k1 v0.1.0 // indirect
+	github.com/bytemare/secret-sharing v0.1.0 // indirect
+	github.com/gtank/ristretto255 v0.1.2 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
